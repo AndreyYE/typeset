381d5ce111f928b53bc36599406d1d7b4cbc431c
AndreyYE <andreidan1986@gmail.com>
2020-02-12 15:16:31 +0300

    final version test typeset

---
+++ indsx.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="style.css">
+
+    <title>Hello, world!</title>
+</head>
+<body>
+<nav>
+    <!--    start logo-->
+    <div id="logo"></div>
+    <div id="logo_title">Остоженка 11</div>
+    <div id="logo_img"></div>
+    <!--    end logo-->
+
+<!--    start bg-->
+    <div id="bg">
+        <div id="ellipse1"></div>
+        <div id="ellipse2"></div>
+        <div id="Rectangle-3_7"></div>
+        <div id="Rectangle-3_6"></div>
+        <div id="Rectangle-3_5"></div>
+        <div id="Rectangle-3_4"></div>
+        <div id="Rectangle-3_3"></div>
+        <div id="Rectangle-3_2"></div>
+        <div id="Rectangle-3_1"></div>
+    </div>
+    <!--    end bg-->
+<!--    start close-->
+    <div class="close"></div>
+<!--    end close-->
+<!--    start items menu-->
+    <a href="#" class="menu-item menu-item-1">О проекте</a>
+    <a href="#" class="menu-item-down menu-item-down-1">О жилом комплексе</a>
+    <a href="#" class="menu-item menu-item-2">Архитектура</a>
+    <a href="#" class="menu-item-down menu-item-down-2">Описание архитектурного стиля</a>
+    <a href="#" class="menu-item menu-item-3">Галерея</a>
+    <a href="#" class="menu-item-down menu-item-down-3">Фото отделки и фасада</a>
+    <a href="#" class="menu-item menu-item-4">Квартиры</a>
+    <a href="#" class="menu-item-down menu-item-down-4">Предложения о продаже</a>
+    <a href="#" class="menu-item menu-item-5">Презентация</a>
+    <a href="#" class="menu-item-down menu-item-down-5">Видео и pdf презентаци</a>
+    <a href="#" class="menu-item menu-item-6">Контакты</a>
+    <a href="#" class="menu-item-down menu-item-down-6">Обратная связь</a>
+<!--    end items menu-->
+</nav>
+
+</html>
